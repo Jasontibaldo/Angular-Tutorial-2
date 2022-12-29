de669b753fae99ef7541b91517ec0b589293b106
Jason Tibaldo <jason.tibaldo@gmail.com>
2022-12-29 16:36:38 -0500

    Messing with component customization

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,8 +2,14 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  template: `
+  <app-server></app-server>
+  <app-server></app-server>`,
+  //styleUrls: ['./servers.component.css']
+  styles: [`
+  h3{
+    color: green;
+  }`]
 })
 export class ServersComponent implements OnInit {
 
